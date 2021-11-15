d0fea9f89d68af049165f98a511ae86e86d30dfe
Daniel Díaz <69810853+danield413@users.noreply.github.com>
2021-11-15 15:07:25 -0500

    SE REESTRUCTURÓ EL PROYECTO - MEJORADO

---
+++ menuOptions/imprimirSalario.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import matplotlib.pyplot as plt
+
+def imprimirSalario(ventana, volverAtras):
+    ventana.title('Estadísticas')
+    ventana.geometry("650x500")
+    ventana.anchor("center")
+    ventana.config(bg="#323232") 
+
+    cedulaEmpleado = StringVar()
+    
+    def calcular():
+        #calcular salario
+        print("calcular")
+
+    def imprimir():
+        #imprimir
+        print("imprimir")
+
+    Label(ventana, text="Gastos Empresariales", bg="#323232", fg="white", font=("Verdana", 13)).grid(pady=10, row=0, column=0)
+
+    Label(ventana, text="Cédula del empleado", fg="white", bg="#258787", font=('Verdana', 10, 'bold')).grid(pady=10, row=1, column=0, padx=10)
+    Entry(ventana, textvariable=cedulaEmpleado).grid(pady=5, row=1, column=1)
+    Button(ventana, text="Calcular", command=calcular, width="20", bg="#5D00FF", relief="flat", fg="white", font=('Verdana', 10, 'bold'), cursor="hand2").grid(pady=20, row=2, column=0, columnspan=2)
+    
+    Label(ventana, text="Pensión", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=3, column=0)
+    Label(ventana, text="pension").grid(pady=5, row=3, column=1)
+    
+    Label(ventana, text="ICBF", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=4, column=0)
+    Label(ventana, text="ICBF").grid(pady=5, row=4, column=1)
+
+    Label(ventana, text="Sueldo", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=5, column=0)
+    Label(ventana, text="SUELDO").grid(pady=5, row=5, column=1)
+
+    Label(ventana, text="Auxilio transporte", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=6, column=0)
+    Label(ventana, text="AUX TRANSP").grid(pady=5, row=6, column=1)
+
+    Label(ventana, text="Bonificaciones", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=7, column=0)
+    Label(ventana, text="BONIFICACIONES").grid(pady=5, row=7, column=1)
+
+    Button(ventana, text="Atrás", width="20", bg="gray", fg="white", relief="flat", font=('Verdana', 10, 'bold'), cursor="hand2", command=volverAtras).grid(row=8, column=0)
+    Button(ventana, text="Imprimir", width="20", bg="gray", fg="white", relief="flat", font=('Verdana', 10, 'bold'), cursor="hand2", command=imprimir).grid(row=8, column=1)
+   
---
+++ menuOptions/ingresarInformacionLaboral.py
@@ -0,0 +1,54 @@
+from tkinter import *
+from functions.comprobarLongitud import comprobarLongitud
+from tkinter import messagebox
+
+def ingresarInformacionLaboral(ventana, volverAtras):
+    ventana.title('Ingresar información laboral del empleado')
+    ventana.geometry("650x500")
+    ventana.anchor("center")
+    ventana.config(bg="#323232") 
+
+    cedulaEmpleado = StringVar()
+    horasTrabajadas = IntVar()
+    horasExtrasTrabajadas = IntVar()
+    horasNoTrabajadas = IntVar()
+    numeroVentas = IntVar()
+
+    def limpiar():
+        cedulaEmpleado.set('')
+        horasTrabajadas.set('')
+        horasExtrasTrabajadas.set('')
+        horasNoTrabajadas.set('')
+        numeroVentas.set('')
+
+    def verificar():
+       if(len(cedulaEmpleado) > 7):
+           #se verifica que el empleado esté ya registradp
+            print('verificando...')
+        
+            
+
+    Label(ventana, text="Información laboral", bg="#323232", fg="white", font=("Verdana", 13)).grid(pady=10, row=0, column=0)
+
+    Label(ventana, text="Cédula del empleado", fg="white", bg="#258787", font=('Verdana', 10, 'bold')).grid(pady=10, row=1, column=0, padx=10)
+    Entry(ventana, textvariable=cedulaEmpleado).grid(pady=5, row=1, column=1)
+
+    Label(ventana, text="Horas trabajadas", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=2, column=0)
+    Entry(ventana, textvariable=horasTrabajadas).grid(pady=5, row=2, column=1)
+    
+    Label(ventana, text="Horas extras trabajadas", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=3, column=0)
+    Entry(ventana, textvariable=horasExtrasTrabajadas).grid(pady=5, row=3, column=1)
+
+    Label(ventana, text="Horas No trabajadas", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=4, column=0)
+    Entry(ventana, textvariable=horasNoTrabajadas).grid(pady=5, row=4, column=1)
+
+    Label(ventana, text="Horas No trabajadas", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=4, column=0)
+    Entry(ventana, textvariable=horasNoTrabajadas).grid(pady=5, row=4, column=1)
+
+    Label(ventana, text="Número de ventas", bg="#323232", fg="#fff", font=("Verdana", 10)).grid(pady=5, row=5, column=0)
+    Entry(ventana, textvariable=numeroVentas).grid(pady=5, row=5, column=1)
+
+    Button(ventana, text="Atrás", width="20", bg="gray", fg="white", relief="flat", font=('Verdana', 10, 'bold'), cursor="hand2", command=volverAtras).grid(row=6, column=0)
+    Button(ventana, text="Guardar información", command=verificar, width="20", bg="#5D00FF", relief="flat", fg="white", font=('Verdana', 10, 'bold'), cursor="hand2").grid(pady=20, row=6, column=1)
+
+    
